43cca504257c8a4acb736fbf9e1a396d8e0dce81
youssef-dev <youssef@offshore-digital-marketing.com>
2024-05-05 17:42:08 +0100

    cc

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,16 +1,18 @@
 'use client';
 
-import { useState } from "react";
+import { useEffect, useState, useTransition } from "react";
 import { createClient } from '@supabase/supabase-js'
 
-const supabase_url = 'https://rxhngregjdeukgydbwlt.supabase.co'
-const anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ4aG5ncmVnamRldWtneWRid2x0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ0OTI3OTQsImV4cCI6MjAyMDA2ODc5NH0.pGuO8b1KZjHvTSkA3iAoQ_k5mkE5nX-x-m-unAT7cxw'
+const supabase_url = 'https://fzqhueqhjhuhrixycabl.supabase.co'
+const anon_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6cWh1ZXFoamh1aHJpeHljYWJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ5MjUxMDMsImV4cCI6MjAzMDUwMTEwM30.Yk1uOX5qwc1vL2K72ruVp-x0ncM2keGndxwMs2vgw4w'
 
 const supabase = createClient(supabase_url, anon_key)
 
 const Page = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isPending, startTransition] = useTransition();
+    const [login, setLogin] = useState(false);
 
     const HandleEmail = (e) => {
         setEmail(e.target.value);
@@ -32,14 +34,38 @@ const Page = () => {
           
     };
 
+    const handleLogin = async () => {
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+          })
+          console.log(data, error)
+    }
+
+    useEffect(() => {
+        startTransition(async () => {
+            const {data} = await supabase.auth.getUser();
+            console.log(data);
+            if (data.user)
+                setLogin(true)
+            else
+                setLogin(false)
+        })
+    },[])
+
+
     return (
         <div>
             <h1>Sign Up</h1>
+            {isPending ? <p>Loading...</p> : null}
             <form onSubmit={handleSubmit}>
                 <input type="email" placeholder="email" onChange={HandleEmail}/>
                 <input type="text" placeholder="password" onChange={handlePassword}/>
                 <button type="submit">Sign Up</button>
+
             </form>
+                <button onClick={handleLogin}>Sign</button>
+            {login ? <p>Logged in</p> : <p>Not logged in</p>}
         </div>
     );
 };
